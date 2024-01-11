d1ec47726222d2824d84403c1fd397ba22a4456b
NainIglesias <naioptamo00@gmail.com>
2024-01-11 10:03:55 +0100

    updating navBar

--- script.js
+++ script.js
@@ -4,9 +4,13 @@ const themeIcon = document.getElementById('themeIcon');
 const informationIcon = document.getElementById('informationIcon');
 const homeIcon = document.getElementById('homeIcon');
 
-// Cambio de light/dark themez
-boton.addEventListener("click", () => {
-    //alert('¡Soy un botón!');  
+const themeIconMobile = document.getElementById('themeIconMobile');
+const informationIconMobile = document.getElementById('informationIconMobile');
+const homeIconMobile = document.getElementById('homeIconMobile');
+
+// Cambio de light/dark themes
+function changeThemes(){
+    //alert("aaaaaaaaaaaa")
     const newCss = colorCss.getAttribute('href') === 'darkStyles.css' ? 'lightStyles.css' : 'darkStyles.css';
     colorCss.setAttribute('href', newCss);
     const newThemeIcon =  themeIcon.getAttribute('src') === 'src/moonIcon.png' ? 'src/sunIcon.png':'src/moonIcon.png';
@@ -16,7 +20,15 @@ boton.addEventListener("click", () => {
     const newHomeIcon =  homeIcon.getAttribute('src') === 'src/lightHome.png' ? 'src/darkHome.png':'src/lightHome.png';
     homeIcon.setAttribute('src',newHomeIcon);
 
-});
+    const newthemeIconMobile =  themeIconMobile.getAttribute('src') === 'src/moonIcon.png' ? 'src/sunIcon.png':'src/moonIcon.png';
+    themeIconMobile.setAttribute('src',newthemeIconMobile);
+    const newInformationIconMobile =  informationIconMobile.getAttribute('src') === 'src/lightInformation.png' ? 'src/darkInformation.png':'src/lightInformation.png';
+    informationIconMobile.setAttribute('src',newInformationIconMobile);
+    const newHomeIconMobile =  homeIconMobile.getAttribute('src') === 'src/lightHome.png' ? 'src/darkHome.png':'src/lightHome.png';
+    homeIconMobile.setAttribute('src',newHomeIconMobile);
+
+}
+boton.addEventListener("click", changeThemes);
  // Peticiones a la API de monster hunter.
 const xhr = new XMLHttpRequest();
 xhr.open("GET", "https://mhw-db.com/monsters");
@@ -30,9 +42,9 @@ xhr.onload = () => {
         if(a.name < b.name){return -1;}
         return 0;
     });
-    console.log(data);
 } else {
     console.log(`Error: ${xhr.status}`);
 }
 };
 xhr.send();
+console.log(data);
